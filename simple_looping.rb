20ff0404c24fdab1da087c14144debec35e2dbc2
Theo Weller <weller.theodor@gmail.com>
2019-02-20 21:19:07 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -42,6 +42,6 @@ end
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   for item in 1..number_of_times
-    puts phrase"
+    puts phrase
   end
 end

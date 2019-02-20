43a3caeec0b327df603be7249da49dc14a0799a7
Theo Weller <weller.theodor@gmail.com>
2019-02-20 21:12:22 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -41,7 +41,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  count = 1..number_of_times
+  count = 0..number_of_times
   for count in number_of_times
     puts "#{phrase}"
   end
